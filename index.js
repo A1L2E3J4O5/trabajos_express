2843b99b6fc974abc0da2656d10c6aafc37b6638
jesus ramirez <135658843+A1L2E3J4O5@users.noreply.github.com>
2023-11-13 15:51:11 -0500

    project4

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 const listViewRouter = require('./list-view-router');
 const listEditRouter = require('./list-edit-router');
 
-const task = [
-    {
-        id: '123456',
-        isCompleted: false,
-        descripcion: "walk the dog"
-    },
-]
-app.get('/task', (req, res) => {
-    res.json(task);
-  });
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use('/list-view', listViewRouter);
-app.use('/list-edit',listEditRouter);
 
-app.listen(port,()=>
-{
-    console.log('Servidor Express escuchando en puerto: ', port);
-})
+dotenv.config();
+
+app.use(express.json());
+
+
+const users = [
+  { id: 1, username: 'user1', password: 'password1' },
+  { id: 2, username: 'user2', password: 'password2' },
+];
+
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({ error: 'Credenciales inválidas' });
+  }
+
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+
+  res.json({ token });
+});
+
+const verifyToken = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token no proporcionado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Token inválido' });
+  }
+};
+
+app.use('/list-view', verifyToken, listViewRouter);
+app.use('/list-edit', verifyToken, listEditRouter);
+
+app.listen(port, () => {
+  console.log('Servidor Express escuchando en puerto:', port);
+});
